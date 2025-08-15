cd9ab49b7f1388415270a67f0d1a143d11070adf
BradleyMauncho <bmauncho@gmail.com>
2025-08-15 13:57:49 +0300

    updateButtons

--- Assets/GameAssets/Scripts/Mainscene/Button/ButtonController.cs
+++ Assets/GameAssets/Scripts/Mainscene/Button/ButtonController.cs
@@ -2,18 +2,49 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.Events;
 using DG.Tweening;
+using UnityEngine.UI;
 
 
 public class ButtonController : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
     public  bool isPointerDown = false;
     public bool isPersistentGlow;
+    public bool isInteractable = true;
+    private bool lastInteractableValue;
+
+    public Sprite baseSprite;
+    public Sprite normalsprite;
+
+    public GameObject ButtonNormal;
     public GameObject ButtonGlow;
 
     public UnityEvent onClick;
 
+    private Image buttonNormalImage;
+
+    private void Awake ()
+    {
+        if (ButtonNormal != null)
+            buttonNormalImage = ButtonNormal.GetComponent<Image>();
+
+        HandleInteractableChanged(isInteractable); // Initialize visuals
+    }
+
+    private void Update ()
+    {
+        if (lastInteractableValue != isInteractable)
+        {
+            HandleInteractableChanged(isInteractable);
+            lastInteractableValue = isInteractable;
+        }
+    }
+
     public void OnPointerDown ( PointerEventData eventData )
     {
+        if(!isInteractable)
+        {
+            return; // Ignore pointer down if not interactable
+        }
         isPointerDown = true;
         if(ButtonGlow != null)
         {
@@ -22,15 +53,18 @@ public class ButtonController : MonoBehaviour, IPointerDownHandler, IPointerUpHa
                 if(ButtonGlow.activeSelf)
                 {
                     ButtonGlow.SetActive(false);
+                    ButtonNormal.SetActive(true);
                 }
                 else
                 {
                     ButtonGlow.SetActive(true);
+                    ButtonNormal.SetActive(false);
                 }
             }
             else
             {
                 ButtonGlow.SetActive(true);
+                ButtonNormal.SetActive(false);
             }
                
         }
@@ -38,14 +72,43 @@ public class ButtonController : MonoBehaviour, IPointerDownHandler, IPointerUpHa
 
     public void OnPointerUp ( PointerEventData eventData )
     {
+        if (!isInteractable)
+        {
+            return; // Ignore pointer up if not interactable
+        }
         if (ButtonGlow != null)
         {
             if (!isPersistentGlow)
             {
                 ButtonGlow.SetActive(false);
+                ButtonNormal.SetActive(true);
             }
         }
         onClick?.Invoke();
         isPointerDown = false;
-    } 
+    }
+    private void HandleInteractableChanged ( bool newValue )
+    {
+        if (buttonNormalImage != null)
+        {
+            buttonNormalImage.sprite = newValue ? normalsprite : baseSprite;
+        }
+
+        if (ButtonGlow != null)
+        {
+            ButtonGlow.SetActive(false);
+        }
+    }
+
+    [ContextMenu("Test - 0ff")]
+    public void InteractableOff ()
+    {
+        isInteractable = false;
+    }
+
+    [ContextMenu("Test - On")]
+    public void InteractableOn ()
+    {
+        isInteractable = true;
+    }
 }
